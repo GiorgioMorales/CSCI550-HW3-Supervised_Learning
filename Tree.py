b0855e9ac1eeb10d99ccd84a68add65415b952a3
Kyle Webster <kyle1234webster@gmail.com>
2020-11-05 16:35:43 -0700

    Finished Decision Tree generation. TODO: Classification method

--- Tree.py
+++ Tree.py
@@ -1,6 +1,7 @@
 from utils import *
 import pandas as pd
 from collections import Counter
+import math
 import heapq
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -31,23 +32,28 @@ def most_common_class(votes: list):
                     Class Definition
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-class node:
-    def __init__(self, c, split=None):
-        self.c = c
+
+
+class Decision:
+    def __init__(self, split=None, left=None, right=None):
+        if right is None:
+            right = []
+        if left is None:
+            left = []
         self.split = split
-        self.child = []
+        self.right = right
+        self.left = left
 
-    def getVal(self):
-        if self.split:
-            return self.split
-        else:
-            return self.c
+    def addLeft(self, l):
+        self.left = l
 
-    def getLeft(self):
-        return self.child[0]
+    def addRight(self, r):
+        self.right = r
 
-    def getRight(self):
-        return self.child[1]
+
+class Leaf:
+    def __init__(self, c):
+        self.c = c
 
 
 class Split:
@@ -59,30 +65,72 @@ class Split:
 class Tree:
     """Decision Tree Classifier"""
 
-    def __init__(self):
+    def __init__(self, eda, pi):
         """Decision Tree constructor
         """
-        self.node = None
+        self.eda = eda
+        self.pi = pi
 
-    def evaluate(self, data, X):
-        return 1,2
 
-    def tree(self, trainData: np.ndarray, eda, pi):
-        data = trainData.tolist()
-        n = len(data[:-1])
-        y, nj = np.unique(data[-1], return_counts=True)
-        purity = np.max(nj/n)
-        if n <= eda or purity >= pi:
-            return y[np.argmax(nj/n)]
+    def gain(self, D, Dy, Dn, k):
+        temp, pD = np.unique(D[:, -1], return_counts=True)
+        temp, pY = np.unique(Dy[:, -1], return_counts=True)
+        temp, pN = np.unique(Dn[:, -1], return_counts=True)
+        hD = 0
+        hDy = 0
+        hDn = 0
+        for i in pD:
+            hD += i/len(D)*math.log2(i/len(D))
+        for i in pY:
+            hDy += i/len(Dy)*math.log2(i/len(Dy))
+        for i in pN:
+            hDn += i/len(Dn)*math.log2(i/len(Dn))
+        hD *= -1
+        hDy *= -1
+        hDn *= -1
+        temp = len(Dy)/len(D)*hDy + len(Dn)/len(D)*hDn
+        return hD - temp
+
+    def evaluate(self, data, X, xi, c):
+        data = data[X.argsort()]
+        M = []
+        ni = np.zeros(len(c))
+        Nvi = []
+        for j in range(len(data) - 1):
+            ni[c.tolist().index(data[j][-1])] += 1
+            if X[j + 1] != X[j]:
+                v = X[j] + (X[j + 1] - X[j]) / 2
+                M.append(v)
+                Nvi.append(ni.copy())
+        ni[c.tolist().index(data[-1][-1])] += 1
+        value = None
+        score = 0
+        for v in M:
+            Dy = data[np.where(data[:, xi] <= v)[0]]
+            Dn = data[np.where(data[:, xi] > v)[0]]
+            s = self.gain(data, Dy, Dn, len(c))
+            if s > score:
+                value = v
+                score = s
+        return value, score
+
+    def tree(self, trainData: np.ndarray):
+        data = trainData
+        n = len(data)
+        y, nj = np.unique(data[:, -1], return_counts=True)
+        purity = np.max(nj / n)
+        if n <= self.eda or purity >= self.pi:
+            return y[np.argmax(nj / n)]
         split = None
         score = 0
         for i in range(len(data[:-1][0])):
-            v, s = self.evaluate(trainData, data[:, i])
+            v, s = self.evaluate(trainData, data[:, i], i, y)
             if s > score:
                 split = Split(i, v)
                 score = s
-        DY = np.take(data, np.argwhere(data[:, split.xi] <= split.v))
-
+        DY = data[np.where(data[:, split.xi] <= split.v)[0]]
+        DN = data[np.where(data[:, split.xi] > split.v)[0]]
+        return Decision(split=split, left=self.tree(DY), right=self.tree(DN))
 
     def classify(self, tree, testData: np.ndarray):
         """Estimate the class of each of the data point in the testData array"""
@@ -95,6 +143,7 @@ class Tree:
             classifications.append([int(sample.tolist()[-1]), estimate])
         return classifications
 
+
 if __name__ == '__main__':
     # Load Image Segmentation dataset
     datas = readDataset()
@@ -106,16 +155,15 @@ if __name__ == '__main__':
     # Try different values of k (4 - 10)
     f1scores = np.zeros((kval, 7))
     count = 0
-    for k in range(4, 11):
-        # Initialize Decision Tree object
-        tree_classifier = Tree()
-        tree = tree_classifier.tree(trainData=train, eda=5, pi=0.1)
-        # Cross-validation
-        f1sc = []
-        for v in range(kval):
-            # Get the prediction results for the validation set
-            results = np.array(tree_classifier.classify(train[v], test[v]))
-            f1sc.append(f1_score(results[:, 0], results[:, 1], len(np.unique(results[:, 0]))))
+    # Initialize Decision Tree object
+    tree_classifier = Tree(eda=5, pi=0.7)
+    # Cross-validation
+    f1sc = []
+    for v in range(kval):
+        tree = tree_classifier.tree(trainData=train[v])
+        # Get the prediction results for the validation set
+        results = np.array(tree_classifier.classify(tree, test[v]))
+        f1sc.append(f1_score(results[:, 0], results[:, 1], len(np.unique(results[:, 0]))))
 
         f1scores[:, count] = f1sc
         count += 1
